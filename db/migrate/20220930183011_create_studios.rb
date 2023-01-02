18fd76b2bd601d4cf4a624ab1e65ce198bc2526d
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2023-01-02 11:00:42 -0600

    setup rails gems, spec_helper, and migrations

--- db/migrate/20220930183011_create_studios.rb
+++ db/migrate/20220930183011_create_studios.rb
@@ -3,6 +3,9 @@ class CreateStudios < ActiveRecord::Migration[5.2]
     create_table :studios do |t|
       t.string :name
       t.string :location
+
+      t.timestamps
+
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,23 +10,45 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_30_183033) do
+ActiveRecord::Schema.define(version: 2023_01_02_165656) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "actors", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "movie_actors", force: :cascade do |t|
+    t.bigint "movie_id"
+    t.bigint "actor_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["actor_id"], name: "index_movie_actors_on_actor_id"
+    t.index ["movie_id"], name: "index_movie_actors_on_movie_id"
+  end
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
     t.string "creation_year"
     t.string "genre"
     t.bigint "studio_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["studio_id"], name: "index_movies_on_studio_id"
   end
 
   create_table "studios", force: :cascade do |t|
     t.string "name"
     t.string "location"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "movie_actors", "actors"
+  add_foreign_key "movie_actors", "movies"
   add_foreign_key "movies", "studios"
 end
